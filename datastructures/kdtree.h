4e1cda5c9b97515a8e039a4bbe8447bb2b75009b
Diptorup Deb <diptorup@cs.unc.edu>
2012-12-10 00:31:11 -0500

    Making changes to the kd_tree distance function.
    Add a new paramter to the signature of the kd_tree distance function. The
    void* would be used to pass the prrts_system_t.space_config object.
    prrts_system_t would store the pointer to the PRRTstar object, which would
    be passed to the static function PRRTstar::dist_func and inturn used to call
    the member distanceFunction. Crazy way to handle C callback from C++.

--- datastructures/kdtree.h
+++ datastructures/kdtree.h
@@ -11,7 +11,7 @@
 #include "../utils/crc.h"
 #endif
 
-typedef double (*kd_dist_func)(const double *a, const double *b);
+typedef double (*kd_dist_func)(void* system, const double *a, const double *b);
 typedef void (*kd_near_callback)(void *data, int no, void *value, double dist);
 
 typedef struct kd_tree {
@@ -21,6 +21,7 @@ typedef struct kd_tree {
     struct kd_node *root;
     kd_dist_func dist_func;
     tl_mempool_t mempool;
+    void * space_config;
 } kd_tree_t;
 
 kd_tree_t *kd_create_tree(size_t dimensions
@@ -28,7 +29,8 @@ kd_tree_t *kd_create_tree(size_t dimensions
                         , const double *max
                         , kd_dist_func dist_func
                         , const double *root_config
-                        , void *root_value);
+                        , void *root_value
+                        , void * space_config);
             
 void kd_insert(kd_tree_t *t, const double *config, void *value);
 
